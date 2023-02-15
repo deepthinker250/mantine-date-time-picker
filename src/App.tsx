9f27631d07924dcd755a5f33091952d082010f98
Akash187 <kumarakashseth007@gmail.com>
2023-02-15 20:23:20 +0530

    feat: added margin top to time-picker and code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Box, MantineProvider } from "@mantine/core";
-import DateTimePicker from "./components/DateTimePicker";
-import dayjs from "dayjs";
+import React from 'react'
+import { Box, MantineProvider } from '@mantine/core'
+import DateTimePicker from './components/DateTimePicker'
+import dayjs from 'dayjs'
 
 function App() {
-  return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Box p="md">
-        <DateTimePicker
-          withAsterisk
-          label="Date Time Picker"
-          placeholder="Pick date time"
-          defaultValue={new Date()}
-          //autoHideNow={false}
-          minDate={dayjs(new Date()).subtract(6, "days").toDate()}
-          maxDate={dayjs(new Date()).add(6, "days").toDate()}
-        />
-      </Box>
-    </MantineProvider>
-  );
+	return (
+		<MantineProvider withGlobalStyles withNormalizeCSS>
+			<Box p="md">
+				<DateTimePicker
+					withAsterisk
+					label="Date Time Picker"
+					placeholder="Pick date time"
+					defaultValue={new Date()}
+					//autoHideNow={false}
+					minDate={dayjs(new Date()).subtract(6, 'days').toDate()}
+					maxDate={dayjs(new Date()).add(6, 'days').toDate()}
+				/>
+			</Box>
+		</MantineProvider>
+	)
 }
 
-export default App;
+export default App
